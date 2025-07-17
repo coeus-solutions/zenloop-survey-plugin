76cbe785d95879966e37335b428373fa5296a7b2
ajmal <ajmal.khan@workhub.ai>
2025-07-17 17:25:03 +0500

    Fix bugs

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -11,7 +11,7 @@ export const loader = async ({ request }) => {
   const url = new URL(request.url);
 
   if (url.searchParams.get("shop")) {
-    throw redirect(`/plan`);
+    throw redirect(`/app?${url.searchParams.toString()}`);
   }
 
   return { showForm: Boolean(login) };
